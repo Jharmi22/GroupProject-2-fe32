35875eedeb25af09716107987404431a83f03224
Irwansyah <114460269+Irwnsyh@users.noreply.github.com>
2022-11-21 01:16:05 +0800

    Update login.jsx

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
-import logo from '../../assets/images/logo.png';
 import "./LoginStyle.css"
 
 export default class Login extends Component {
